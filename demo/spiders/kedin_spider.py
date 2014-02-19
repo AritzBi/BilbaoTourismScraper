74b914d6ee8ba850f75816eedec966bbcdb25764
Perurena <aritzbi@gmail.com>
2014-02-19 02:33:37 +0100

    Kedin almost finished

--- demo/spiders/kedin_spider.py
+++ demo/spiders/kedin_spider.py
@@ -9,6 +9,7 @@ from scrapy.http.request import Request
 
 class KedinSpider(XMLFeedSpider):
 	name="kedin_spider"
+	BASE='http://www.kedin.es'
 	allowed_domains=["kedin.es"]
 	start_urls=["http://kedin.es/vizcaya/conciertos-de-pop/feed.rss","http://kedin.es/vizcaya/conciertos-de-rock/feed.rss","http://kedin.es/vizcaya/conciertos-de-cantautores.html","http://kedin.es/vizcaya/clasica/feed.rss","http://kedin.es/vizcaya/conciertos-de-electronica/feed.rss","http://kedin.es/vizcaya/conciertos-de-indie.html","http://kedin.es/vizcaya/conciertos-de-musica/feed.rss","http://kedin.es/vizcaya/festivales/feed.rss","http://kedin.es/vizcaya/conciertos-de-heavy/feed.rss","http://kedin.es/vizcaya/conciertos-de-jazz/feed.rss"]
 	itertag = 'item'
@@ -31,16 +32,46 @@ class KedinSpider(XMLFeedSpider):
 	def parse_events_links(self,response):
 		sel=Selector(response)
 		event=sel.xpath("//article/ul[@class='data-info']")
-		#print event.extract()
 		startDate=event.xpath("li[@class='info']/p[@class='lcontainer']/strong/meta[@itemprop='startDate']/@content").extract()
+		startDate=startDate.pop()
 		endDate=event.xpath("li[@class='info']/p[@class='lcontainer']/strong/meta[@itemprop='endDate']/@content").extract()
+		category=sel.xpath("//header/div['@id=breadcrumb']/span/a/span/text()").extract()
 		if len(endDate)>0:
-			
-		else:
-			endHour=event.xpath("li[@class='time']/p[@class='lcontainer']/strong/text()").re(r"\d{2}[:]\d{2}")
-			if len(endHour)==2:
+			hours=event.xpath("li[@class='info']/p[@class='lcontainer']/div[@class='recurrence-info']/strong/text()").re(r"\d{2}[:]\d{2}")
+			if len(hours)==2:
 				startHour=endHour[0]
 				endHour=endHour.pop()
-				endDate=startDate.pop().replace(startHour,endHour)
-				print endDate
+		else:
+			hours=event.xpath("li[@class='time']/p[@class='lcontainer']/strong/text()").re(r"\d{2}[:]\d{2}")
+			if len(hours)==2:
+				startHour=hours[0]
+				endHour=hours.pop()
+				endDate=startDate.replace(startHour,endHour)
+		item = response.meta['item']
+		item['startDate']=startDate
+		item['endDate']=endDate
+		item['startHour']=startHour
+		item['endHour']=endHour
+		item['category']=category
+		locationURL=event.xpath("li[@class='place'][@itemprop='location']/p[@class='lcontainer']/a/@href").extract()
+		if len(locationURL)>0:
+			request=Request(self.BASE+locationURL[0],callback=self.parse_event_location)
+			request.meta['item']=item
+			yield request
+
+	def parse_event_location(self,response):
+		sel=Selector(response)
+		location=sel.xpath("//body/div/div/article/div/div/section[@id='place']")
+		locationName=location.xpath("p/strong/text()").extract()
+		locationAdress=location.xpath("p[@class='address']/text()").extract()
+		lat=location.xpath("div[@id='map']/@data-lat").extract()
+		lon=location.xpath("div[@id='map']/@data-lng").extract()
+		if len(locationName)>0:
+			print "Location: "+locationName.pop()
+		if len(locationAdress)>0:
+			print "Location Adress: "+locationAdress.pop()
+		if len(lat)>0:
+			print "Latitude: "+lat.pop()
+		if len(lon)>0:
+			print "Longitude: "+lon.pop()
 
