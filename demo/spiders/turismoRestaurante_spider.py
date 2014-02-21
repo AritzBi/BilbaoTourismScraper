72b74abb6d69366e53ca7427b67675edfdbb590c
Perurena <aritzbi@gmail.com>
2014-02-21 03:30:00 +0100

    Restaurants scaping started

--- demo/spiders/turismoRestaurante_spider.py
+++ demo/spiders/turismoRestaurante_spider.py
@@ -6,20 +6,21 @@ from scrapy.selector import Selector
 class RestaurantesSpider(Spider):
 	BASE='http://turismo.euskadi.net'
 	name='restaurantes_spider'
-	allowed_domains=['turismoa.euskadi.net']
+	allowed_domains=['turismo.euskadi.net']
 	start_urls=['http://turismo.euskadi.net/x65-15634x/es/s12PortalWar/buscadoresJSP/buscadorB1.jsp?r01kLang=es&marks=4&restorationType=R']
 
 	def parse(self,response):
 		sel = Selector(response)
 		otherURLS = sel.xpath('//*[@id="containerConRutas"]/div[3]/div/div/div/div/div[3]/div/ul')
 		relativeURLS=otherURLS.xpath("li[@class='r01NavBarItem r01NavBarIntermediateItem']/a/@href").extract()
-		relativeURLS.append(otherURLS.xpath("li[@class='r01NavBarItem']/a/@href").extract())
-		moreLinksPage=otherURLS.xpath("li[@class='r01NavBarItem r01NavBarIntermediateItem']/a/@href").extract()
-		print relativeURLS
-		"""for relativeLink in relativeURLS:
+		#EL ultimo de la lista tiene una clase diferente.
+		lastRelativeURL=otherURLS.xpath("li[@class='r01NavBarItem']/a/@href").extract()
+		if len(lastRelativeURL)>0:
+			relativeURLS.append(lastRelativeURL.pop())
+		moreLinksPage=otherURLS.xpath("li[@class='r01NavBarCtrlItem r01NavBarNextBlock']/a/@href").extract()
+		for relativeLink in relativeURLS:
 			url=self.BASE+relativeLink
 			print url
-			yield Request(url,callback=self.parse_restaurants_links)"""
+			yield Request(url,callback=self.parse_restaurants_links)
 
-	def parse_restaurants_links(self, response):
-		print response.url
+	def parse_restaurants_links(self,response):
